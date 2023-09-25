95d3f9323735258591ba4ad9f4ce29ae5310abc3
Brandon D Zarate Estrada <bdzarate@ucdavis.edu>
2023-09-25 16:45:16 -0700

    filtered games by platform

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,20 +1,34 @@
-import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react"
-import { BsChevronDoubleDown } from "react-icons/bs"
-import usePlatforms from "../hooks/usePlatforms"
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { BsChevronDoubleDown } from "react-icons/bs";
+import usePlatforms from "../hooks/usePlatforms";
+import { Platform } from "../hooks/useGames";
 
-
-const PlatformSelector = () => {
-    const { data, error } = usePlatforms();
-
-    if (error) return null;
-  return (
-    <Menu>
-        <MenuButton as ={Button} rightIcon={<BsChevronDoubleDown/>}>Platforms</MenuButton>
-        <MenuList>
-            {data.map(platform => <MenuItem key={platform.id}>{platform.name}</MenuItem>)}
-        </MenuList>
-    </Menu>
-  )
+interface Props {
+  onSelectPlatform: (platform: Platform) => void;
+  selectedPlatform: Platform | null;
 }
 
-export default PlatformSelector
+const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
+  const { data, error } = usePlatforms();
+
+  if (error) return null;
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<BsChevronDoubleDown />}>
+        {selectedPlatform?.name || "Platoforms"}
+      </MenuButton>
+      <MenuList>
+        {data.map((platform) => (
+          <MenuItem
+            onClick={() => onSelectPlatform(platform)}
+            key={platform.id}
+          >
+            {platform.name}
+          </MenuItem>
+        ))}
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default PlatformSelector;
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,19 +2,27 @@ import useData from "./useData";
 import { Genre } from "./useGenres";
 
 export interface Platform {
-    id: number,
-    name: string,
-    slug: string,
+  id: number;
+  name: string;
+  slug: string;
 }
-export interface Game {            //interface for the game object provided by the api documention
-    id: number,
-    name: string,
-    background_image: string,
-    parent_platforms: { platform: Platform }[],
-    metacritic: number,
+export interface Game {
+  //interface for the game object provided by the api documention
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
 }
 
-
-const useGames = (selectedGenre: Genre | null) => useData<Game>('/games', { params: { genres: selectedGenre?.id}}, [selectedGenre?.id])
+const useGames = (
+  selectedGenre: Genre | null,
+  selectedPlatform: Platform | null
+) =>
+  useData<Game>(
+    "/games",
+    { params: { genres: selectedGenre?.id, platforms: selectedPlatform?.id} },
+    [selectedGenre?.id, selectedPlatform?.id]
+  );
 
 export default useGames;
