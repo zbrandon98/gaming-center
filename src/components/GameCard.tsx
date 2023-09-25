94da70dc85d3741c3c194fcb718f1e55d63d8c2b
Brandon D Zarate Estrada <bdzarate@ucdavis.edu>
2023-09-25 12:04:10 -0700

    optimized gameCard images with cropped images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,9 @@ import { Card, CardBody, HStack, Heading, Image } from '@chakra-ui/react'
 import { Game } from '../hooks/useGames' 
 import PlatformIconList from './PlatformIconList'
 import CriticScore from './CriticScore'
+import getCroppedImageUrl from '../services/image-url'
+
+
 
 interface Props {
     game: Game
@@ -10,7 +13,7 @@ interface Props {
 const GameCard = ({game}:Props) => {
   return (
     <Card borderRadius={10} overflow='hidden'>
-        <Image src={game.background_image} alt={game.name}/>
+        <Image src={getCroppedImageUrl(game.background_image)} alt={game.name}/>
         <CardBody>
             <Heading fontSize='2xl'>{game.name}</Heading>
             <HStack marginY={1} justifyContent='space-between'>
