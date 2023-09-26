26d433546ad86ed1e3941f0fa3aa82f35191c926
Brandon D Zarate Estrada <bdzarate@ucdavis.edu>
2023-09-25 20:26:46 -0700

    added search bar layout

--- src/components/NabBar.tsx
+++ src/components/NabBar.tsx
@@ -1,12 +1,14 @@
 import { HStack, Image} from "@chakra-ui/react"
 import wsLogo from "../assets/logo.webp"
 import ColorModeSwitch from "./ColorModeSwitch"
+import SearchInput from "./SearchInput"
 
 
 const NabBar = () => {
   return (
-    <HStack justifyContent="space-between" padding="10px">
+    <HStack padding="10px">
         <Image src={wsLogo} alt="Logo of websit" boxSize="60px"/>
+        <SearchInput/>
         <ColorModeSwitch/>
     </HStack>
   )
