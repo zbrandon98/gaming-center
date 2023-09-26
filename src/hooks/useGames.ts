c18447d712d039e0d031965b4056ee57e62e6735
Brandon D Zarate Estrada <bdzarate@ucdavis.edu>
2023-09-25 17:15:54 -0700

    refactored query objects

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,6 @@
+import { GameQuery } from "../App";
 import useData from "./useData";
-import { Genre } from "./useGenres";
+
 
 export interface Platform {
   id: number;
@@ -16,13 +17,14 @@ export interface Game {
 }
 
 const useGames = (
-  selectedGenre: Genre | null,
-  selectedPlatform: Platform | null
+    gameQuery: GameQuery
 ) =>
   useData<Game>(
     "/games",
-    { params: { genres: selectedGenre?.id, platforms: selectedPlatform?.id} },
-    [selectedGenre?.id, selectedPlatform?.id]
+    { params: { 
+        genres: gameQuery.genre?.id, 
+        platforms: gameQuery.platform?.id} },
+    [gameQuery] 
   );
 
 export default useGames;
